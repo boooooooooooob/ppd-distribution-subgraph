0b394cf0d2eb89354f34f0421b1baed9433105f3
bob <>
2024-03-11 16:06:10 +0800

    update schema

--- src/token-distribution.ts
+++ src/token-distribution.ts
@@ -1,30 +1,8 @@
 import {
-  Initialized as InitializedEvent,
   Invested as InvestedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   TokensClaimed as TokensClaimedEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/TokenDistribution/TokenDistribution"
-import {
-  Initialized,
-  Invested,
-  OwnershipTransferred,
-  TokensClaimed,
-  Upgraded
-} from "../generated/schema"
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+} from '../generated/TokenDistribution/TokenDistribution'
+import { Invested, TokensClaimed } from '../generated/schema'
 
 export function handleInvested(event: InvestedEvent): void {
   let entity = new Invested(
@@ -41,22 +19,6 @@ export function handleInvested(event: InvestedEvent): void {
   entity.save()
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handleTokensClaimed(event: TokensClaimedEvent): void {
   let entity = new TokensClaimed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
@@ -64,19 +26,6 @@ export function handleTokensClaimed(event: TokensClaimedEvent): void {
   entity.investor = event.params.investor
   entity.period = event.params.period
   entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
