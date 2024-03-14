df47d7f31252d97ae76a24408132f1d9ec6c2380
bob <>
2024-03-14 18:50:12 +0800

    add summary for periods

--- src/token-distribution.ts
+++ src/token-distribution.ts
@@ -2,7 +2,8 @@ import {
   Invested as InvestedEvent,
   TokensClaimed as TokensClaimedEvent,
 } from '../generated/TokenDistribution/TokenDistribution'
-import { Invested, TokensClaimed } from '../generated/schema'
+import { Invested, TokensClaimed, PeriodSummary } from '../generated/schema'
+import { Bytes, BigInt } from '@graphprotocol/graph-ts'
 
 export function handleInvested(event: InvestedEvent): void {
   let entity = new Invested(
@@ -17,6 +18,22 @@ export function handleInvested(event: InvestedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let periodSummary = PeriodSummary.load(
+    Bytes.fromI32(event.params.period.toI32())
+  )
+  if (periodSummary == null) {
+    periodSummary = new PeriodSummary(
+      Bytes.fromI32(event.params.period.toI32())
+    )
+    periodSummary.totalInvested = BigInt.fromI32(0)
+    periodSummary.totalClaimed = BigInt.fromI32(0)
+  }
+
+  periodSummary.totalInvested = periodSummary.totalInvested.plus(
+    event.params.amount
+  )
+  periodSummary.save()
 }
 
 export function handleTokensClaimed(event: TokensClaimedEvent): void {
@@ -32,4 +49,20 @@ export function handleTokensClaimed(event: TokensClaimedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let periodSummary = PeriodSummary.load(
+    Bytes.fromI32(event.params.period.toI32())
+  )
+  if (periodSummary == null) {
+    periodSummary = new PeriodSummary(
+      Bytes.fromI32(event.params.period.toI32())
+    )
+    periodSummary.totalInvested = BigInt.fromI32(0)
+    periodSummary.totalClaimed = BigInt.fromI32(0)
+  }
+
+  periodSummary.totalClaimed = periodSummary.totalClaimed.plus(
+    event.params.amount
+  )
+  periodSummary.save()
 }
